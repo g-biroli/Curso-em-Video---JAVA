064c93394f58c148d70a288190e65bb3a1285585
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-02-06 23:54:04 +0100

    Ejercicio de hoy Clase UE

---
+++ AlquillerC/src/AlquillerC/AlquilerCoches.java
@@ -0,0 +1,12 @@
+package AlquillerC;
+
+//criando os atributos
+public class AlquilerCoches {
+	private int PrecioDiario;
+	private int Descuento;
+	
+public AlquilerCoches ( int PrecioDiario, int descuento ) {
+		this.PrecioDiario = PrecioDiario;
+		this.Descuento = Descuento;
+	}
+}
