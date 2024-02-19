2ee6eda45b1a9f47ac2613b057e321a2dc265dc2
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-02-19 16:25:30 +0100

    cm

--- ejemplosHerencia/src/ejemplosHerencia/Profesor.java
+++ ejemplosHerencia/src/ejemplosHerencia/Profesor.java
@@ -1,5 +1,22 @@
 package ejemplosHerencia;
 
-public class Profesor {
+public class Profesor extends Persona{
+	protected double salario;
+
+	protected Profesor(String dni, String nombre, double salario) {
+		super(dni, nombre);
+		this.salario = salario;
+	}
+
+	protected double getSalario() {
+		return salario;
+	}
+
+	protected void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	
+	
 
 }
