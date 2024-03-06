e91da10b01d271e3e84e09bc0551d9dae17f457e
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-03-06 18:42:52 +0100

    teste

--- ejemplosHerencia/src/ejemplosHerencia/Alumna.java
+++ ejemplosHerencia/src/ejemplosHerencia/Alumna.java
@@ -2,13 +2,13 @@ package ejemplosHerencia;
 
 public class Alumna extends Persona{
 	protected String numExp;
+	protected String nombre;
 	
 	protected Alumna(String dni, String nombre, String numExp) {
 		super(dni, nombre);
 		this.numExp = numExp;
 	}
-	//comentario para atualizar commit teste. Problema no app
-	//teste
+	
 
 	public String getNumExp() {
 		return numExp;
@@ -16,6 +16,10 @@ public class Alumna extends Persona{
 	
 	public void setNumExp(String numExp) {
 		this.numExp = numExp;
+	}
+	
+	public void getNombre (String nombre) {
+		this.nombre = nombre;
 	}
 //metodo com void é um metodo sem retorno, o valor nao é tao importante, é um valor que nao precisa ser armazenado
 //já um metodo com return, é um metodo que o resultado da variavel string, int, ... será armazenado e possivelmente usado futuramente.
